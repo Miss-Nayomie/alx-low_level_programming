c1ca8049fada7696faf02629685a1616a2e6fd9e
Miss-Nayomie <naominyongesa2017@gmail.com>
2023-02-27 10:00:58 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int i, j, max;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	max = i;
+	j = max / 2;
+	while (j <= max)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
